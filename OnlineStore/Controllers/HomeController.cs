102fd39b37215739058400fdc232829b205fe838
ahmed mohamed <ahmeda70@hotmail.com>
2024-09-05 16:14:11 +0400

    ep=> 18

--- OnlineStore/Controllers/HomeController.cs
+++ OnlineStore/Controllers/HomeController.cs
@@ -6,12 +6,12 @@ namespace OnlineStore.Controllers
 {
     public class HomeController : Controller
     {
+        SouqcomContext db = new SouqcomContext();
        
-
         public IActionResult Index()
-        {
-            SouqcomContext db = new SouqcomContext();
+        {           
             var cats = db.Catigories.ToList();
+            ViewBag.prod = db.Products.ToList();
             return View(cats);
         }
 
@@ -22,9 +22,31 @@ namespace OnlineStore.Controllers
 
         public IActionResult Cat()
         {
-            SouqcomContext db = new SouqcomContext();
             var cats =  db.Catigories.ToList();
             return View(cats);
+        }
+
+        public IActionResult Products(int id)
+        {
+            var prods = db.Products.Where(x => x.CatId == id).ToList();
+            ViewBag.Cat = db.Catigories.Where(x => x.Id == id).ToList();
+            return View(prods);
+        }
+
+        public IActionResult ProductDetails(int id)
+        {
+            var prod = db.Products.Where(x => x.Id == id).ToList();
+            return View(prod);
+        }
+
+        [HttpGet]
+        public IActionResult ProductSearch(string pname)
+        {
+            var prod = db.Products.Where(x => x.Name.Contains(pname)).ToList();
+            ViewBag.pname = pname;
+            return View(prod);
+
+
         }
 
         public IActionResult Cart()
